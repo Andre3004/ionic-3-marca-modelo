0509e69da08bda6f30e6501f12ef23d8d74b3324
André Luiz Damasceno <andre.luiz@eits.com.br>
2018-07-19 17:58:03 -0300

    Alterado para rodar no servidor

--- src/main/ts/src/providers/utils/app-settings.ts
+++ src/main/ts/src/providers/utils/app-settings.ts
@@ -2,8 +2,8 @@ import { HttpHeaders } from "@angular/common/http";
 
 export class AppSettings 
 {
-    //public static API_URL = 'https://ionicmarcamodelo.herokuapp.com/api'; //servidor
-    public static API_URL = 'http://192.168.25.236:8080/api'; //local
+    public static API_URL = 'https://ionicmarcamodelo.herokuapp.com/api'; //servidor
+    //public static API_URL = 'http://192.168.25.236:8080/api'; //local
 
     public static httpOptions = {
         headers: new HttpHeaders({
